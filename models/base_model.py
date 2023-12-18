2ca5e27ae808766181064aac6e3dc40167e43afa
Anthony Osigbe <thronesofglory@gmail.com>
2023-12-18 03:29:57 -0800

    Bug fix

--- models/base_model.py
+++ models/base_model.py
@@ -85,10 +85,11 @@ class BaseModel:
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
 
-        if my_dict['_sa_instance_state']:
-            my_dict.pop('_sa_instance_state')
+        if '_sa_instance_state' in my_dict:
+            del my_dict['_sa_instance_state']
 
         return my_dict
+
 
     def delete(self):
         """Deletes the current instance from
